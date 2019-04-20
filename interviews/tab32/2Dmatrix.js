b5d045a4319cd73eae18867142de6dac57d6a9a3
Rohan <duarohan18@gmail.com>
2019-04-20 10:21:23 +0530

    new questions

---
+++ interviews/tab32/2Dmatrix.js
@@ -0,0 +1,12 @@
+var matrix = 
+[
+    [1,2,3,4],
+    [5,6,7,8],
+    [9,10,11,12]
+] 
+for (i=0; i< matrix.length; i++){
+    console.log(matrix[0][i])
+}
+
+// 1,2,3,4,8,12,11,10,9,5,6,7
+// for (i=0; i<matrix[i][j])
